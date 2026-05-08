f5b2fddac832a7b7492d23db20848178adab380f
Patrick Stephens <pat@telemetryforge.io>
2026-05-08 12:00:44 +0100

    feat: manual review and update of missing changes
    Signed-off-by: Patrick Stephens <pat@telemetryforge.io>

--- source/src/config_format/flb_cf_yaml.c
+++ source/src/config_format/flb_cf_yaml.c
@@ -2243,7 +2243,8 @@ static int consume_event(struct flb_cf *conf, struct local_ctx *ctx,
                 state = state_push_variant(ctx, state, 0);
             }
             else if (strcmp(state->key, "routes") == 0 ||
-                     strcmp(state->key, "processors") == 0) {
+                     strcmp(state->key, "processors") == 0 ||
+                     strcmp(state->key, "filters") == 0) {
                 state = state_push_variant(ctx, state, 0);
             }
             else {
@@ -2256,30 +2257,6 @@ static int consume_event(struct flb_cf *conf, struct local_ctx *ctx,
             }
             break;
         case YAML_MAPPING_START_EVENT:
-
-            if (strcmp(state->key, "processors") == 0) {
-                struct flb_cf_group *group;
-
-                group = flb_cf_group_create(conf, state->cf_section,
-                                             state->key,
-                                             strlen(state->key));
-
-                if (group == NULL) {
-                    flb_error("unable to create processors group");
-                    return YAML_FAILURE;
-                }
-
-                state->cf_group = group;
-                state = state_push(ctx, STATE_INPUT_PROCESSORS);
-
-                if (state == NULL) {
-                    flb_error("unable to allocate state");
-                    return YAML_FAILURE;
-                }
-
-                break;
-            }
-
             if (state->section == SECTION_PROCESSOR || state->section == SECTION_FILTER) {
                 /* when in a processor or filter section, allow plugins to have nested
                  * properties which are returned as a cfl_variant */
@@ -2293,7 +2270,8 @@ static int consume_event(struct flb_cf *conf, struct local_ctx *ctx,
             }
 
             if (strcmp(state->key, "routes") == 0 ||
-                strcmp(state->key, "processors") == 0) {
+                strcmp(state->key, "processors") == 0 ||
+                strcmp(state->key, "filters") == 0) {
                 state = state_push_variant(ctx, state, 1);
 
                 if (state == NULL) {
